60ba36d5449ee9699b7e0aab60277d5bbdbd49dd
dipendra <deardipen50@gmail.com>
2020-10-17 23:37:18 +0545

    added modals for profile edit

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -36,20 +36,80 @@ class UserProfile extends React.Component {
         console.log(`Edit ${this.state.user}`)
     }
 
+    handleNameChange = (e) => {
+        this.setState({
+            userName: e.target.value
+        })
+    }
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    //Send post request from these functions
+    updateName = e => {
+        console.log(e)
+    }
+
+    updateEmail = e => {
+        console.log(e)
+    }
+
     render() {
         let buttons;
         if (this.state.user) {
             buttons = <>
-                <button className="btn btn-primary mx-3" onClick={() => this.handleEdit(this.state.user)}>Edit</button>
+                <button className="btn btn-primary mx-3" >Edit</button>
+
                 <button className="btn btn-danger mx-3" onClick={() => this.handleDelete(this.state.user)}>Delete Profile</button>
             </>
         }
+
+
         return (
             <div>
                 <div className="jumbotron container my-3">
-                    <h2 className="display-3">{this.state.userName}</h2>
+                    <h2 className="display-3">{this.state.userName} <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Edit</button></h2>
+                    <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <input className="form-control" type="text" onChange={this.handleNameChange} value={this.state.userName} />
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary" onClick={this.updateName}>Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <hr className="my-4" />
-                    <p><strong>Email:</strong>{this.state.email}</p>
+                    <p><strong>Email:</strong> {this.state.email} <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal2">Edit</button></p>
+                    <div className="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <input className="form-control" type="text" onChange={this.handleEmailChange} value={this.state.email} />
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary" onClick={this.updateEmail}>Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     {buttons}
                 </div>
                 <div className="container">
