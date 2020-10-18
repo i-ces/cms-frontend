e424ddbb69b4581ef2f6d9f92a87525e70ca7fa7
Dhurba Baral <you@example.com>
2020-10-18 10:40:38 +0545

    integration for profile edit

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,3 +1,187 @@
+// import React, { useEffect, useState } from 'react'
+// import PostCard from './PostCard'
+// import axios from "axios"
+
+
+
+// function UserProfile() {
+//     // state = {
+//     //     user: '',
+//     //     userName: '',
+//     //     email: ''
+//     // };
+//     const [user,setUser]=useState("")
+//     const [userName,setuserName]=useState("")
+//     const [email,setEmail]=useState("")
+
+//     // componentDidMount() {
+//     //     const config = {
+//     //         params: {
+//     //             _id: localStorage.getItem('_id')
+//     //         }
+//     //     }
+
+//     //     this.setState({
+//     //         user: config.params._id
+//     //     })
+//     //     axios.get(`http://localhost:4000/users/${config.params._id}`).then((res) => {
+//     //         //console.log(res.data.name)
+//     //         this.setState({ userName: res.data.name, email: res.data.email })
+//     //     }).catch((e) => {
+//     //         console.log(e)
+//     //     })
+//     // }
+
+//     useEffect(()=>{
+//         const config = {
+//                     params: {
+//                         _id: localStorage.getItem('_id')
+//                     }
+//                 }
+//                 setUser(config.params._id)
+//                 axios.get(`http://localhost:4000/users/${config.params._id}`).then((res) => {
+//                             //console.log(res.data.name)
+//                             // this.setState({ userName: res.data.name, email: res.data.email })
+//                             setuserName(res.data.name)
+//                             setEmail(res.data.email)
+//                         }).catch((e) => {
+//                             console.log(e)
+//                          })
+                
+//     })
+    
+//     const handleDelete =()=> {
+//         const config = {
+//             headers: {
+//                 Authorization: 'Bearer ' + localStorage.getItem('token')
+//             }
+//         }
+//         window.confirm("Are you sure you wish to delete this item?") &&
+//         axios.delete('http://localhost:4000/users/self',config).then((res)=>{
+//             console.log(res)
+//             localStorage.clear()
+//         }).catch((error)=>{
+//             console.log(error)
+//         })
+//     }
+//     // const handleEdit = e => {
+//     //     console.log(`Edit ${user}`)
+//     // }
+
+//     // const handleNameChange = (e) => {
+//     //     // this.setState({
+//     //     //     userName: e.target.value
+//     //     // })
+//     //     setuserName(e.value)
+//     // }
+//     // const handleEmailChange = (e) => {
+//     //     // this.setState({
+//     //     //     email: e.target.value
+//     //     // })
+//     //     setEmail(e.value)
+//     // }
+//     //Send request from these functions
+//     const updateName = () => {
+//         const config = {
+//             headers: {
+//                 Authorization: 'Bearer ' + localStorage.getItem('token')
+//             }
+//         }
+//         axios.patch('http://localhost:4000/users/self',config).then((res)=>{
+//             console.log(res)
+//             // this.setState({ userName: res.data.name, email: res.data.email })
+//             setuserName(res.data.name)
+
+//         }).catch((error)=>{
+//             console.log(error)
+//         })
+//     }
+
+//     const updateEmail = () => {
+//         const config = {
+//             headers: {
+//                 Authorization: 'Bearer ' + localStorage.getItem('token')
+//             }
+//         }
+//         axios.patch('http://localhost:4000/users/self',config).then((res)=>{
+//             console.log(res)
+//             // this.setState({ userName: res.data.name, email: res.data.email })
+//             setEmail(res.data.email)
+//         }).catch((error)=>{
+//             console.log(error)
+//         })
+//     }
+
+    
+//         let buttons;
+//         if (user) {
+//             buttons = <>
+//                 <button className="btn btn-primary mx-3" >Edit</button>
+
+//                 <button className="btn btn-danger mx-3" onClick={handleDelete()}>Delete Profile</button>
+//             </>
+//         }
+
+//         return (
+//             <div>
+//                 <div className="jumbotron container my-3">
+//                     <h2 className="display-3">{userName} <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Edit</button></h2>
+//                     <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//                         <div className="modal-dialog">
+//                             <div className="modal-content">
+//                                 <div className="modal-header">
+//                                     <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+//                                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+//                                         <span aria-hidden="true">&times;</span>
+//                                     </button>
+//                                 </div>
+//                                 <div className="modal-body">
+//                                     <input className="form-control" type="text" onChange={(e) => setuserName(e.target.value)} value={userName} />
+//                                 </div>
+//                                 <div className="modal-footer">
+//                                     <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+//                                     <button type="button" className="btn btn-primary" onClick={updateName()}>Save changes</button>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <hr className="my-4" />
+//                     <p><strong>Email:</strong> {email} <button className="btn btn-primary" data-toggle="modal" data-target="#exampleModal2">Edit</button></p>
+//                     <div className="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//                         <div className="modal-dialog">
+//                             <div className="modal-content">
+//                                 <div className="modal-header">
+//                                     <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+//                                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+//                                         <span aria-hidden="true">&times;</span>
+//                                     </button>
+//                                 </div>
+//                                 <div className="modal-body">
+//                                     <input className="form-control" type="text" onChange={(e) => setEmail(e.target.value)} value={email} />
+//                                 </div>
+//                                 <div className="modal-footer">
+//                                     <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+//                                     <button type="button" className="btn btn-primary" onClick={updateEmail()}>Save changes</button>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     {buttons}
+//                 </div>
+//                 <div className="container">
+//                     <h2 className="display-5 my-5">Posts Created by User</h2>
+//                     <PostCard />
+//                 </div>
+//             </div>
+        
+//     )
+        
+//     }
+
+
+
+// export default UserProfile
+
 import React from 'react'
 import PostCard from './PostCard'
 import axios from "axios"
@@ -28,21 +212,21 @@ class UserProfile extends React.Component {
             console.log(e)
         })
     }
-    
-    handleDelete = e => {
-        const config = {
-            headers: {
-                Authorization: 'Bearer ' + localStorage.getItem('token')
+
+    handleDelete =()=> {
+                const config = {
+                    headers: {
+                        Authorization: 'Bearer ' + localStorage.getItem('token')
+                    }
+                }
+                window.confirm("Are you sure you wish to delete this item?") &&
+                axios.delete('http://localhost:4000/users/self',config).then((res)=>{
+                    console.log(res)
+                    localStorage.clear()
+                }).catch((error)=>{
+                    console.log(error)
+                })
             }
-        }
-        window.confirm("Are you sure you wish to delete this item?") &&
-        axios.delete('http://localhost:4000/users/self',config).then((res)=>{
-            console.log(res)
-            localStorage.clear()
-        }).catch((error)=>{
-            console.log(error)
-        })
-    }
     handleEdit = e => {
         console.log(`Edit ${this.state.user}`)
     }
@@ -58,8 +242,30 @@ class UserProfile extends React.Component {
         })
     }
     //Send post request from these functions
-    updateName = e => {
-        console.log(e)
+    updateName = ()=> {
+                    axios.patch('http://localhost:4000/users/self').then((res)=>{
+                        console.log(res)
+                        this.setState({ userName: res.data.name, email: res.data.email })
+            
+                    }).catch((error)=>{
+                        console.log(error)
+                    })
+
+                    const config = {
+                        params: {
+                            _id: localStorage.getItem('_id')
+                        }
+                    }
+            
+                    this.setState({
+                        user: config.params._id
+                    })
+                    axios.get(`http://localhost:4000/users/${config.params._id}`).then((res) => {
+                        //console.log(res.data.name)
+                        this.setState({ userName: res.data.name, email: res.data.email })
+                    }).catch((e) => {
+                        console.log(e)
+                    })
     }
 
     updateEmail = e => {
@@ -72,11 +278,9 @@ class UserProfile extends React.Component {
             buttons = <>
                 <button className="btn btn-primary mx-3" >Edit</button>
 
-                <button className="btn btn-danger mx-3" onClick={() => this.handleDelete(this.state.user)}>Delete Profile</button>
+                <button className="btn btn-danger mx-3" onClick={() => this.handleDelete()}>Delete Profile</button>
             </>
         }
-
-
         return (
             <div>
                 <div className="jumbotron container my-3">
@@ -130,9 +334,5 @@ class UserProfile extends React.Component {
             </div>
         )
     }
-
 }
-
-
-
 export default UserProfile
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import axios from "axios"
+
+axios.defaults.headers.common['Authorization']='Bearer '+localStorage.getItem('token')
 
 ReactDOM.render(
   <React.StrictMode>
